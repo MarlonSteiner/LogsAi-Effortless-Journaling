a5287283bf917b0df77eaf65e04602860ed46b4a
Marlon Steiner <marlon.steiner1999@yahoo.com>
2025-07-30 20:54:01 +0200

    fixed scss bug in development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,4 +75,15 @@ Rails.application.configure do
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
 
+  # config/environments/development.rb
+  Rails.application.configure do
+    # compile assets on the fly
+    config.assets.compile = true
+
+    # keep fingerprinting so helpers still resolve correctly
+    config.assets.digest = true
+
+    # disable debug so your JS is still concatenated
+    config.assets.debug = false
+  end
 end
